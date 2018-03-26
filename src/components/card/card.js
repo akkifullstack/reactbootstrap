958c868427cda75b60392c298dbf6cdda4d4fc8d
akkifullstack <37371723+akkifullstack@users.noreply.github.com>
2018-03-26 17:34:17 +0530

    adding graph image

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,7 +8,7 @@ export default class Card extends React.Component{
             <div>
             <h1>Card</h1>
             <div className="card">
-                <div className="card-header"><img src="http://schoolleavers.co.za/example/DEVELOPER.jpg" width="516" height="360"/></div>
+                <div className="card-header"><img src="./image/map.jpg" width="516" height="360"/></div>
                 <div className="card-body">Akshay</div>
                 <div className="card-footer">Full Stack Developer</div>
             </div>
